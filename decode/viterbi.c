78cd4673966fb3d99e0de50d6dd0aee5286d6f79
Morgan Diepart <morgandiepart@gmail.com>
2025-03-12 10:01:07 +0100

    Fixed an incorrent memset length leading to bit corruption at the end of decoded payloads

--- decode/viterbi.c
+++ decode/viterbi.c
@@ -162,7 +162,7 @@ uint32_t viterbi_chainback(uint8_t* out, size_t pos, uint16_t len)
     uint8_t state = 0;
     size_t bitPos = len+4;
 
-    memset(out, 0, (len-1)/8+1);
+    memset(out, 0, (bitPos/8)+1);
 
     while(pos > 0)
     {
